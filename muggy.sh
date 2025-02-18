0a56dce484ef0b63e3490cc807d6566fbf356c36
theDAOS <sanjusanthosh46@gmail.com>
2025-02-18 14:54:45 +0530

    git added

--- muggy.sh
+++ muggy.sh
@@ -4,15 +4,23 @@ addData () {
     random=$((1 + RANDOM % 10))
     temp=""
     temp+="$random \n"
-    for ((i=1; i<=random; i++))
+    for ((tempLoop=1; tempLoop<=random; tempLoop++))
     do 
         temp+="mug "
     done
-    return "$temp";
+    echo -e "$temp"
 }
+
+
+date --set="10 days ago"
+
+git init
 
 for ((i=1; i<=10; i++))
 do
-    echo "$addData" >> mug.txt
+    addData >> mug.txt
+    git add -all
+    git commit -m "$date"
+    date -s "+1 day"
     sleep 5
 done
